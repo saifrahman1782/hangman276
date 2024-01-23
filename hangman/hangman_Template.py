0a4b482cc12c780a37745a57b6fbe6ef348d6d74
saifrahman1782 <saifrahman1782@outlook.com>
2024-01-23 22:39:45 +0000

    Quite a few changes which is the complete game.
    Realised that self.num_letters should be assigned to the length of the set of the self.word attribute. This is because sets do not allow duplicates which is what my previous code allowed.
    
    check_letter function completed which checks if the letter is in the word. This was done by enumerating the self.word attribute. Enumerating it allows the code to iterate over the characters in self.word and get the corresponding indices for them. Players num_lives is decreased everytime they guess the incorrect letter.
    
    ask_letter function completed which asks the user for a letter and checks if the letter has already been tried and if the character is a single character. As an extra detail i've also made the code check that the letter isalpha() and will prompt the user to enter a letter if they do not.
    
    play_game function completed which checks whether the player has correctly guessed the letters and will either give the player a congratulations or a you lost print depending on which case is true.
    
    Comments for code all added to explain my thought process too.

--- hangman/hangman_Template.py
+++ hangman/hangman_Template.py
@@ -43,12 +43,13 @@ class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
-        self.word = random.choice(word_list)
-        self.word_guessed = ['_'] * len(self.word)
-        self.num_letters = len(self.word)
-        self.list_letters = []
+        self.word = random.choice(word_list) #randomly selects a word
+        self.word_guessed = ['_'] * len(self.word) # replaces the word to be guessed characters with a '_'
+        self.num_letters = len(set(self.word)) # This sets the self.word as a set for the num_letters attribute in order to ensure there are no duplicate letters and therefore the letters checked are unique.
+        self.list_letters = [] #empty array of letters that the player has already tried.
         print(f"The mystery word has {self.num_letters} characters")
-        print(f"{self.word_guessed}")
+        print(f"{self.word_guessed}") # prints out the word to be guessed which will show in the format: ['_','_','_','_','_']
+
         # TODO 2: Initialize the attributes as indicated in the docstring
         # TODO 2: Print two message upon initialization:
         # 1. "The mistery word has {num_letters} characters"
@@ -67,6 +68,14 @@ class Hangman:
             The letter to be checked
 
         '''
+        letter = letter.lower()
+        if letter in self.word: # checks if the entered letter is in the word.
+            for i, char in enumerate(self.word): # here I have enumerated self.word in order to iterate over each character and also get the corresponding indices for each character.
+                if char == letter: # checking if the character is equal to the entered letter
+                    self.word_guessed[i] = letter
+        else:
+            self.num_lives -= 1 # decrements the players number of lives if they guess the wrong letter.
+
         # TODO 3: Check if the letter is in the word. TIP: You can use the lower() method to convert the letter to lowercase
         # TODO 3: If the letter is in the word, replace the '_' in the word_guessed list with the letter
         # TODO 3: If the letter is in the word, the number of UNIQUE letters in the word that have not been guessed yet has to be reduced by 1
@@ -81,6 +90,17 @@ class Hangman:
         2. If the character is a single character
         If it passes both checks, it calls the check_letter method.
         '''
+        while True: #use a while true statement here to ensure that this check is repeated until the user enters the correct input
+            letter = input("Please enter a letter: ").lower()
+            if len(letter) == 1 and letter.isalpha() and letter not in self.list_letters: # a bunch of checks to ensure that the user has entered a valid input.
+                break
+            elif len(letter) > 1: 
+                print("Please, enter just one character")
+            elif letter in self.list_letters:
+                print(f"{letter} was already tried.")
+
+        self.list_letters.append(letter) # adds the letter that the user has entered to the letters list to ensure these letters aren't used again.
+        self.check_letter(letter)
 
         # TODO 1: Ask the user for a letter iteratively until the user enters a valid letter
         # TODO 1: Assign the letter to a variable called `letter`
@@ -92,6 +112,16 @@ class Hangman:
 def play_game(word_list):
     # As an aid, part of the code is already provided:
     game = Hangman(word_list, num_lives=5)
+
+    while game.num_lives > 0 and '_' in game.word_guessed: # a simple check that keeps the game going until these checks are not met
+        game.ask_letter()
+        print(game.word_guessed)
+
+    if '_' not in game.word_guessed: # this checks that the game has replaced all of the '_' characters with the correct characters which indicates that the user has guessed correctly.
+        print("Congratulations! You won!")
+    else:
+        print(f"You lost! The word was {game.word}")
+
     # TODO 1: To test this task, you can call the ask_letter method
     # TODO 2: To test this task, upon initialization, two messages should be printed 
     # TODO 3: To test this task, you call the ask_letter method and check if the letter is in the word
